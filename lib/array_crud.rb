aa32dd3fcbe4bb7cbfda1209fad443d5c196b1b5
Terri Riggle <codeofarmour@outlook.com>
2017-10-04 20:22:32 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array
-  (things_to_be_grateful_for << "arrays!")
+  things_to_be_grateful_for << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
